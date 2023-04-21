6d08aae0feb41ad0d73e2419fb789dfbbcaa2d42
Aden Herold <aden.herold1@gmail.com>
2023-04-21 19:59:11 +1000

    refactor: moved all source files into src directory in Sanity project

--- admin/sanity.config.ts
+++ admin/sanity.config.ts
@@ -1,12 +1,12 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
-import {structure} from './structure'
+import {schemaTypes} from './src/schemas'
+import {structure} from './src/structure'
 
 export default defineConfig({
   name: 'default',
-  title: 'entertainment',
+  title: 'Entertainment',
 
   projectId: '1i8qbox6',
   dataset: 'production',
