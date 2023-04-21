251715705a8d1204fcdc6e53e6d322f98cc0482e
Aden Herold <aden.herold1@gmail.com>
2023-04-21 21:06:21 +1000

    build(deps): added @chakra-ui/storybook-addon dependency and configured Chakra UI for storybook

--- app/.storybook/main.ts
+++ app/.storybook/main.ts
@@ -5,7 +5,11 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@chakra-ui/storybook-addon',
   ],
+  features: {
+    emotionAlias: false,
+  } as any,
   framework: {
     name: '@storybook/nextjs',
     options: {},
