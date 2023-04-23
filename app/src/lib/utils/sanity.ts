ad6a3343a217ee4be36683f0b8119be6c31b630d
Aden Herold <aden.herold1@gmail.com>
2023-04-23 10:04:16 +1000

    refactor: removed useSanityImage hook and repurposed as utility function

--- app/src/lib/utils/sanity.ts
+++ app/src/lib/utils/sanity.ts
@@ -1,7 +1,7 @@
 import { Image } from '@/graphql/codegen/graphql'
 import { ImageProps } from 'next/image'
 
-export default function useSanityImage(image?: Image | null): ImageProps {
+export function extractSanityImage(image?: Image | null): ImageProps {
   const src = image?.asset?.source?.url || ''
   const alt = image?.asset?.altText || ''
 
