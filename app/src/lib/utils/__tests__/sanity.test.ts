5a11a4805e7c3d4938a819a9ac813f2ca01e0439
Aden Herold <aden.herold1@gmail.com>
2023-04-23 12:16:25 +1000

    test: added unit tests for extractSanityImage utility function

---
+++ app/src/lib/utils/__tests__/sanity.test.ts
@@ -0,0 +1,48 @@
+import { Image } from '@/graphql/codegen/graphql'
+import { ImageProps } from 'next/image'
+import { faker } from '@faker-js/faker'
+import { extractSanityImage } from '../sanity'
+
+describe('extractSanityImage', () => {
+  test('extracts NextJS compatible image from Sanity image source', () => {
+    const url = faker.internet.url()
+    const altText = faker.lorem.sentence()
+    const width = +faker.random.numeric(3)
+    const height = +faker.random.numeric(3)
+
+    const sanityImage: Image = {
+      asset: {
+        source: { url },
+        altText,
+        metadata: {
+          dimensions: {
+            width,
+            height,
+          },
+        },
+      },
+    }
+
+    const expected: ImageProps = {
+      src: url,
+      alt: altText,
+      width,
+      height,
+    }
+
+    const result = extractSanityImage(sanityImage)
+    console.log(sanityImage)
+
+    expect(result).toEqual(expected)
+  })
+
+  test('returns default values if image is null or undefined', () => {
+    const sanityImages = [null, undefined]
+    const expected: ImageProps = { src: '', alt: '', width: 0, height: 0 }
+
+    sanityImages.forEach((image) => {
+      const result = extractSanityImage(image)
+      expect(result).toEqual(expected)
+    })
+  })
+})
