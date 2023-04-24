2b702699289e1dd7abbd664e2d0446bf2a0d5138
Aden Herold <aden.herold1@gmail.com>
2023-04-25 09:09:12 +1000

    feat: updated _app page component to wrap rendered component in main element

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
+import theme from '@/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Layout>
-        <Component {...pageProps} />
+        <main>
+          <Component {...pageProps} />
+        </main>
       </Layout>
     </ChakraProvider>
   )
