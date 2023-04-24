3773bc66a0996651e6ce761904584e874aaebd5d
Aden Herold <aden.herold1@gmail.com>
2023-04-25 08:56:08 +1000

    feat: added client module to retrieve Apollo client instance

---
+++ app/src/client.ts
@@ -0,0 +1,16 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+
+let client: ApolloClient<any> | null = null
+
+export default function getClient() {
+  if (!client || typeof window === 'undefined') {
+    client = new ApolloClient({
+      link: new HttpLink({
+        uri: process.env.SANITY_API_URL,
+      }),
+      cache: new InMemoryCache(),
+    })
+  }
+
+  return client
+}
