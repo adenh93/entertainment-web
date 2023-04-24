0e050c5152cde7ebdfdff283176132f2d0a9d222
Aden Herold <aden.herold1@gmail.com>
2023-04-24 12:41:31 +1000

    feat: added MediaDetails UI component

---
+++ app/src/components/MediaDetails/MediaDetails.tsx
@@ -0,0 +1,52 @@
+import { Media } from '@/graphql/codegen/graphql'
+import { HStack, Heading, IconProps, Stack, Text } from '@chakra-ui/react'
+import { TVIcon, FilmIcon } from '@/components/Icons'
+import { formatMediaReleaseYear } from '@/lib/utils/media'
+import MediaMetafields from '@/components/MediaMetafields'
+
+export interface MediaDetailsProps {
+  media: Media
+  size: 'sm' | 'lg'
+}
+
+const MediaTypeDisplay: Record<MediaType, string> = {
+  tvSeries: 'TV Series',
+  movie: 'Movie',
+}
+
+function getMediaIcon(mediaType: MediaType) {
+  const props: IconProps = { width: '12px', height: '12px' }
+
+  switch (mediaType) {
+    case 'tvSeries':
+      return <TVIcon {...props} />
+    case 'movie':
+      return <FilmIcon {...props} />
+    default:
+      return <></>
+  }
+}
+
+export default function MediaDetails({ media, size }: MediaDetailsProps) {
+  const mediaType = media?.mediaType as MediaType
+  const releaseYear = formatMediaReleaseYear(media?.releaseDate)
+  const icon = getMediaIcon(mediaType)
+  const mediaTypeDisplay = MediaTypeDisplay[mediaType] || ''
+
+  const textSize = size === 'lg' ? 'md' : 'sm'
+  const headingSize = size === 'lg' ? 'sm' : 'xs'
+
+  return (
+    <Stack spacing={0.5}>
+      <MediaMetafields>
+        <Text size={textSize}>{releaseYear}</Text>
+        <HStack alignItems="center" spacing={1.5}>
+          {icon}
+          <Text size={textSize}>{mediaTypeDisplay}</Text>
+        </HStack>
+        <Text size={textSize}>{media?.rating?.code}</Text>
+      </MediaMetafields>
+      <Heading size={headingSize}>{media?.title}</Heading>
+    </Stack>
+  )
+}
