eb72b069471bb3556971705f1efbac4d8285b99c
Aden Herold <aden.herold1@gmail.com>
2023-04-24 19:17:39 +1000

    feat: added Navigation component

---
+++ app/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,48 @@
+import { Box, Stack } from '@chakra-ui/react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { ReactElement } from 'react'
+import {
+  BookmarkIcon,
+  BrandIcon,
+  FilmIcon,
+  GridIcon,
+  TVIcon,
+} from '@/components/Icons'
+
+interface NavItem {
+  href: string
+  icon: ReactElement
+}
+
+const navItems: NavItem[] = [
+  { href: '/', icon: <GridIcon /> },
+  { href: '/movies', icon: <FilmIcon /> },
+  { href: '/tv-series', icon: <TVIcon /> },
+  { href: '/bookmarks', icon: <BookmarkIcon /> },
+]
+
+export default function Navigation() {
+  const router = useRouter()
+
+  return (
+    <Stack spacing={7} alignItems="center">
+      <Link href="/">
+        <BrandIcon color="brand" />
+      </Link>
+      <Stack spacing={5.5} alignItems="center">
+        {navItems.map(({ href, icon }) => {
+          const isActive = router.pathname === href
+
+          return (
+            <Link href={href}>
+              <Box as="span" color={isActive ? 'white' : 'icon'}>
+                {icon}
+              </Box>
+            </Link>
+          )
+        })}
+      </Stack>
+    </Stack>
+  )
+}
