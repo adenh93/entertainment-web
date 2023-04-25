49354cd69a9b60b7e6264ffc3a0977cdf7e3ade4
Aden Herold <aden.herold1@gmail.com>
2023-04-25 17:23:48 +1000

    feat: updated index page to add new Search functionality

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,14 +1,32 @@
+import { Stack } from '@chakra-ui/react'
 import { PageAll } from '@/graphql/codegen/graphql'
 import getClient from '@/client'
 import GET_PAGE_ALL_SETTINGS from '@/graphql/queries/getPageAllSettings'
 import MediaPage from '@/components/MediaPage'
+import SearchInput from '@/components/SearchInput'
+import useMediaSearch from '@/hooks/useMediaSearch'
+import SearchResults from '@/components/SearchResults'
 
 export interface HomeProps {
   page: PageAll
 }
 
 export default function Home({ page }: HomeProps) {
-  return <MediaPage pageSettings={page.settings} />
+  const { search, searchResults, onUpdateSearch } = useMediaSearch()
+
+  return (
+    <Stack spacing={2.5}>
+      <SearchInput
+        onChange={onUpdateSearch}
+        placeholder="Search movies or TV series"
+      />
+      {search ? (
+        <SearchResults search={search} searchResults={searchResults} />
+      ) : (
+        <MediaPage pageSettings={page.settings} />
+      )}
+    </Stack>
+  )
 }
 
 export async function getStaticProps() {
