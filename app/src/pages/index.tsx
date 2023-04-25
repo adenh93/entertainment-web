8aa89214106fb684e4e0f86f3dc2791b30d578ca
Aden Herold <aden.herold1@gmail.com>
2023-04-25 10:39:27 +1000

    feat: fleshed out Home page component

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,12 +1,28 @@
-import Head from 'next/head'
+import { PageAll } from '@/graphql/codegen/graphql'
+import getClient from '@/client'
+import GET_PAGE_ALL_SETTINGS from '@/graphql/queries/getPageAllSettings'
+import MediaPage from '@/components/MediaPage'
 
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Entertainment App</title>
-      </Head>
-      <main>Hello world</main>
-    </>
-  )
+export interface HomeProps {
+  page: PageAll
+}
+
+export default function Home({ page }: HomeProps) {
+  return <MediaPage pageSettings={page.settings} />
+}
+
+export async function getStaticProps() {
+  const client = getClient()
+
+  const { data } = await client.query({
+    query: GET_PAGE_ALL_SETTINGS,
+  })
+
+  const page = data.PageAll
+
+  return {
+    props: {
+      page: page,
+    },
+  }
 }
