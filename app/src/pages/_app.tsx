26a6269dc7df6946f3bb0b41cb09d57477599531
Aden Herold <aden.herold1@gmail.com>
2023-04-25 17:13:27 +1000

    feat: updated _app.tsx to wrap app tree in ApolloProvider

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,16 +1,22 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
 import theme from '@/theme'
+import getClient from '@/client'
+
+const client = getClient()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </Layout>
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <main>
+            <Component {...pageProps} />
+          </main>
+        </Layout>
+      </ChakraProvider>
+    </ApolloProvider>
   )
 }
