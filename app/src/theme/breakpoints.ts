6156316e14d4daf4b70dcdf9a7587142c4ef80dc
Aden Herold <aden.herold1@gmail.com>
2023-04-25 20:45:17 +1000

    feat: added breakpoints to custom theme

---
+++ app/src/theme/breakpoints.ts
@@ -0,0 +1,7 @@
+const breakpoints = {
+  sm: '375px',
+  md: '768px',
+  lg: '1440px',
+}
+
+export default breakpoints
--- app/src/theme/index.ts
+++ app/src/theme/index.ts
@@ -2,12 +2,14 @@ import { extendTheme } from '@chakra-ui/react'
 import colors from './colors'
 import space from './spacing'
 import fonts from './fonts'
+import breakpoints from './breakpoints'
 import * as components from './components'
 
 const theme = extendTheme({
   colors,
   space,
   fonts,
+  breakpoints,
   components: {
     ...components,
   },
