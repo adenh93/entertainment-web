e3ff63fa7dd86542e13d69dd9a54f663c75b9e4a
Aden Herold <aden.herold1@gmail.com>
2023-04-25 17:21:39 +1000

    feat: added MediaGrid component

---
+++ app/src/components/MediaGrid/MediaGrid.tsx
@@ -0,0 +1,17 @@
+import { Grid } from '@chakra-ui/react'
+import { Media } from '@/graphql/codegen/graphql'
+import MediaGridItem from '@/components/MediaGridItem'
+
+export interface MediaGridProps {
+  items: Media[]
+}
+
+export default function MediaGrid({ items }: MediaGridProps) {
+  return (
+    <Grid templateColumns="repeat(4, 1fr)" columnGap={5.5} rowGap={4} pr={5}>
+      {items.map((media, key) => (
+        <MediaGridItem media={media} key={key} />
+      ))}
+    </Grid>
+  )
+}
