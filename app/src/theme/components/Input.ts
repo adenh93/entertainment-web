1503cc3bc8b0d2dafc08670788147a833cd9c3d2
Aden Herold <aden.herold1@gmail.com>
2023-04-25 11:35:03 +1000

    feat: added styling for flushed input to custom theme

---
+++ app/src/theme/components/Input.ts
@@ -0,0 +1,28 @@
+import { ComponentStyleConfig } from '@chakra-ui/react'
+
+const Input: ComponentStyleConfig = {
+  defaultProps: {
+    variant: 'flushed',
+  },
+  variants: {
+    flushed: {
+      field: {
+        pb: 1,
+        fontSize: '24px',
+        fontWeight: 300,
+        borderColor: 'transparent',
+        _placeholder: {
+          color: 'white',
+          opacity: 0.5,
+        },
+        _focus: {
+          boxShadow: 'none',
+          borderBottom: '1px solid',
+          borderColor: 'icon',
+        },
+      },
+    },
+  },
+}
+
+export default Input
