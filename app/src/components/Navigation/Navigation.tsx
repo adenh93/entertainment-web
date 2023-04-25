ef78f575d95aa88d7e2b62ed628595557acd0d9e
Aden Herold <aden.herold1@gmail.com>
2023-04-25 20:48:24 +1000

    feat: added responsive styling to Navigation component

--- app/src/components/Navigation/Navigation.tsx
+++ app/src/components/Navigation/Navigation.tsx
@@ -1,21 +1,9 @@
 import { Box, Stack } from '@chakra-ui/react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { ReactElement } from 'react'
-import {
-  BookmarkIcon,
-  BrandIcon,
-  FilmIcon,
-  GridIcon,
-  TVIcon,
-} from '@/components/Icons'
+import { BookmarkIcon, FilmIcon, GridIcon, TVIcon } from '@/components/Icons'
+import NavItem from '@/components/NavItem'
+import Brand from '@/components/Brand'
 
-interface NavItem {
-  href: string
-  icon: ReactElement
-}
-
-const navItems: NavItem[] = [
+const navItems: NavItemType[] = [
   { href: '/', icon: <GridIcon /> },
   { href: '/movies', icon: <FilmIcon /> },
   { href: '/tv-series', icon: <TVIcon /> },
@@ -23,30 +11,33 @@ const navItems: NavItem[] = [
 ]
 
 export default function Navigation() {
-  const router = useRouter()
+  const content = (
+    <Stack
+      spacing={5.5}
+      alignItems="center"
+      direction={{ sm: 'row', lg: 'column' }}
+    >
+      {navItems.map((item) => (
+        <NavItem item={item} />
+      ))}
+    </Stack>
+  )
 
   return (
-    <Stack spacing={7} alignItems="center">
-      <Link href="/">
-        <BrandIcon color="brand" />
-      </Link>
-      <Stack spacing={5.5} alignItems="center">
-        {navItems.map(({ href, icon }) => {
-          const isActive = router.pathname === href
-
-          return (
-            <Link href={href}>
-              <Box
-                as="span"
-                color={isActive ? 'white' : 'icon'}
-                _hover={{ color: isActive ? 'white' : 'brand' }}
-              >
-                {icon}
-              </Box>
-            </Link>
-          )
-        })}
+    <>
+      <Stack
+        spacing={7}
+        alignItems="center"
+        direction={{ sm: 'row', lg: 'column' }}
+        display={{ sm: 'none', lg: 'block' }}
+      >
+        <Brand />
+        {content}
       </Stack>
-    </Stack>
+      <Brand display={{ sm: 'block', lg: 'none' }} />
+      <Box as="span" display={{ sm: 'block', lg: 'none' }}>
+        {content}
+      </Box>
+    </>
   )
 }
